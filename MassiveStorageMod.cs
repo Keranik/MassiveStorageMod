29d7b28e16b4a38fb23303d57801a2acd5082928
Keranik <keranik@gmail.com>
2023-11-26 19:07:09 -0600

    Add project files.

---
+++ MassiveStorageMod.cs
@@ -0,0 +1,26 @@
+﻿using Mafi;
+using Mafi.Base;
+using Mafi.Core;
+using Mafi.Core.Mods;
+using MassiveStorageMod;
+
+namespace MSMod;
+
+public sealed class MSMod : DataOnlyMod 
+{
+
+	public override string Name => "Massive Storage Mod";
+	public override int Version => 1;
+
+	public MSMod(CoreMod coreMod, BaseMod baseMod) 
+	{
+		
+	}
+
+
+	public override void RegisterPrototypes(ProtoRegistrator registrator) 
+	{
+		registrator.RegisterData<MassiveStorages>();
+	}
+
+}
